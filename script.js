2f9f3040575943321d64d334e2077ca3cf685bfe
riyazgithubavailable <144905521+riyazgithubavailable@users.noreply.github.com>
2024-08-31 16:23:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 // complete this js code
-function Person(name, age) {}
+// function Person(name, age) {}
 
-function Employee(name, age, jobTitle) {}
-
+// function Employee(name, age, jobTitle) {}
+class Person {
+	constructor(name,age) {
+		this.name = name;
+		this.age = age;
+	}
+	greet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+	}
+}
+class Employee extends Person {
+	constructor(name,age,jobTitle) {
+		super(name,age)
+		this.jobTitle = jobTitle;
+	}
+	jobGreet(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+	}
+}
+let person = new Person("Alice",25);
+let employe = new Employee("Bob",30,"Manager")
+person.greet()
+employe.jobGreet()
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
